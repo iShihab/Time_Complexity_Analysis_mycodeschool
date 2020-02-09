234a056e72e1d2a8b810b509ee5df6fcccce882f
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-09 08:31:21 +0600

    Update Big-O notation in 5 minutes — The basics.cpp

--- Big-O notation in 5 minutes — The basics.cpp
+++ Big-O notation in 5 minutes — The basics.cpp
@@ -12,3 +12,8 @@
 // for x in range(0,n): ->O(N)
 //  print x; ->O(1)
 //total time = O(1)+O(N) = O(N)
+
+//Example 3: Quadratic(দ্বিঘাত) time
+// for x in range(0,n):
+//      for y in range(O,n):
+//          print x*y; //O(1)
