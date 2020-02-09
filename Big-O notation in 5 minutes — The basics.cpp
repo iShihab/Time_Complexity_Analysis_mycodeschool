1bbf6cb6832411ae3f1e26fa2bef93ddd528f6c1
Sajedur Rahman <58090286+iShihab@users.noreply.github.com>
2020-02-09 08:23:05 +0600

    Update Big-O notation in 5 minutes — The basics.cpp

--- Big-O notation in 5 minutes — The basics.cpp
+++ Big-O notation in 5 minutes — The basics.cpp
@@ -1,3 +1,7 @@
 // O(n!)>O(2^n)>O(n^2)>O(nlogn)>O(n)>O(logn)>O(1)
 // ignore low order terms when higher order is available in the program
 // O(1) Big oh of one which is also constant time
+
+// for x in range(0,n):
+//     print x; // complexity of this line is O(1)
+//complexity of this whole program is N*O(1)
